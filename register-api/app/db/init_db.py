4134438f60249253b1e7eb16f8d75207c48eb904
mike-gangl <michael.e.gangl@jpl.nasa.gov>
2025-04-17 15:01:58 -0700

    Added dockerfile and dockercompose to make deployment easier.

--- register-api/app/db/init_db.py
+++ register-api/app/db/init_db.py
@@ -1,8 +1,24 @@
 from app.core.database import Base, engine
 from app.models.job import Job
 from app.models.application_package_db import ApplicationPackage
+from app.core.config import settings
+
+
 
 def init_db():
+
+    from sqlalchemy import create_engine
+    from sqlalchemy_utils import database_exists, create_database
+
+    engine = create_engine(settings.DATABASE_URL)
+    if not database_exists(engine.url):
+        create_database(engine.url)
+
+    print(database_exists(engine.url))
+
+
+
+
     # Import all models here to ensure they are registered with Base
     from app.models import job
     from app.models import application_package_db
