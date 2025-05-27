0005d43e854f2c2e602ebaec95c90ea7c53ccfb7
Alexa Dye <adye@jpl.nasa.gov>
2025-05-27 13:22:09 -0700

    vpc and outputs

--- dev-env/network.tf
+++ dev-env/network.tf
@@ -7,8 +7,8 @@ module "vpc" {
     name = "aws_vpc"
     cidr = "10.0.0.0/16"
     azs = ["us-west-2a", "us-west-2b"]
-    public_subnets = []
-    private_subnets = []
+    public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+    private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
     enable_dns_hostnames = true
     enable_dns_support = true
 
--- dev-env/outputs.tf
+++ dev-env/outputs.tf
@@ -1,15 +1,42 @@
+# Module VPC
+output "vpc_id" {
+    description = "VPC Id"
+    value = module.vpc.vpc_id
+    sensitive = false
+}
+output "public_subnets" {
+    description = "VPC Public Subnets"
+    value = module.vpc.public_subnets
+    sensitive = false
+}
+output "private_subnets" {
+    description = "VPC Private Subnets"
+    value = module.vpc.private_subnets
+    sensitive = false
+}
+
+# Module EKS
+output "cluster_name" {
+    value = module.eks.cluster_name
+}
+output "kubeconfig" {
+    value = module.eks.kubeconfig
+}
+output "cluster_endpoint" {
+    value = module.eks.cluster_endpoint
+}
+
+# RDS
 output "rds_hostname" {
     description = "RDS instance hostname"
     value = aws_db_instance.catalog.address
     sensitive = true
 }
-
 output "rds_port" {
     description = "RDS instance port"
     value = aws_db_instance.catalog.port
     sensitive = true
 }
-
 output "rds_username" {
     description = "RDS instance root username"
     value = aws_db_instance.catalog.username
