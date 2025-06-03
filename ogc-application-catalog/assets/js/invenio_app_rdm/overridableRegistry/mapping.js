6d53c728d1f06f9276c7943abd6e1db1c425538b
Leonard Kong <lkong@jpl.nasa.gov>
2025-06-03 12:40:22 -0700

    clean install with new version and dependencies

---
+++ ogc-application-catalog/assets/js/invenio_app_rdm/overridableRegistry/mapping.js
@@ -0,0 +1,20 @@
+// This file is part of InvenioRDM
+// Copyright (C) 2023 CERN.
+//
+// Invenio App RDM is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+/**
+ * Add here all the overridden components of your app.
+ */
+
+export const overriddenComponents = {
+	"InvenioAppRdm.Deposit.AccordionFieldFunding.container": ()=> null,
+	"InvenioAppRdm.Deposit.FundingField.container": ()=> null,
+	"InvenioAppRdm.Deposit.AccordionFieldAlternateIdentifiers.container": () => null,
+	"InvenioAppRdm.Deposit.IdentifiersField.container": ()=> null,
+	"InvenioAppRdm.Deposit.AccordionFieldRelatedWorks.container": () => null,
+	"InvenioAppRdm.Deposit.RelatedWorksField.container": ()=> null,
+	"InvenioAppRdm.Deposit.AccordionFieldReferences.container": ()=> null,
+	"InvenioAppRdm.Deposit.ReferencesField.container": ()=> null,
+};
